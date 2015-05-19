11e4af6878bfeea4cb7d7c918c736612cd4444f9
Khalid Hamza <Khalid_hamza@utexas.edu>
2015-05-19 11:38:51 -0500

    Update Problem-5.py

--- Python/Problem-5.py
+++ Python/Problem-5.py
@@ -6,6 +6,7 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+# ---------------incorrect solution so far. Correct solution (done by hand) == 232,792,560------------
 
 def list_builder(n):
 	list = []
